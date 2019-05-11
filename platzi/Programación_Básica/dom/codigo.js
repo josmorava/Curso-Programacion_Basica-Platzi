58c9ba1e9b123196c33fdb04550422578d211b0d
JoseMorales <josemorales-1234@hotmail.es>
2019-05-11 01:42:38 -0400

    Dibujando en el Dom_ Arboleda html

---
+++ platzi/Programación_Básica/dom/codigo.js
@@ -0,0 +1,8 @@
+var ruta = window.location;
+document.write ("Estas en: " + ruta);
+console.log(ruta);
+
+/*para definir una funcion se puede hacer:
+* var = "variable sin parentesis" ó
+*function = "variable sin parentesis"
+*/
