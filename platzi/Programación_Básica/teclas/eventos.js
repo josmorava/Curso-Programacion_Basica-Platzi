e8aacce558731bd89e39f00d6544fe3e4290e17a
JoseMorales <josemorales-1234@hotmail.es>
2019-05-31 01:34:19 -0400

    Dibujando con flechas del teclado_keydown_keyup

--- platzi/Programación_Básica/teclas/eventos.js
+++ platzi/Programación_Básica/teclas/eventos.js
@@ -1,9 +1,12 @@
-/*Interacciones*/
-document.addEventListener("keyup", dibujarTeclado);
-var d = document.getElementById("area_del_dibujo");
+/*Interacciones*/  /*Variables*/
+document.addEventListener("keydown", dibujarTeclado);
 
-/*Variables*/
-var lienzo = d.getContext("2d");
+var cuadrito = document.getElementById("area_del_dibujo");
+
+var papel = cuadrito.getContext("2d");
+
+var x = 150;
+var y = 150;
 
 var teclas = {/*variable dentro de una variable*/
   UP:38,
@@ -11,33 +14,42 @@ var teclas = {/*variable dentro de una variable*/
   LEFT:37,
   RIGHT:39
 };
-console.log(teclas);
 
 
 /*Funciones*/
+
 function dibujarTeclado(evento)
 {
-  switch(evento.keyCode)
+  var colorcito = "yellow";
+   var movimiento = 10;
+  switch(evento.keyCode) /*estructura de toma de desiciones*/
   {
     case teclas.UP:
+    dibujarLinea(colorcito, x, y, x , y - movimiento, papel );
       console.log("arriba");
+      y = y - movimiento;
     break;
     case teclas.DOWN:
+    dibujarLinea(colorcito, x , y , x , y + movimiento, papel);
       console.log("abajo");
+      y = y + movimiento;
     break;
     case teclas.LEFT:
+      dibujarLinea(colorcito, x, y , x - movimiento, y, papel);
       console.log("izquierda");
+      x = x - movimiento;
     break;
     case teclas.RIGHT:
+      dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
       console.log("derecha");
+      x = x + movimiento;
     break;
     default:
       console.log("otra tecla");
     break;
   }
 }
-
-function dibujarLinea(color, xinicial , yinicial , xfinal , yfinal)
+function dibujarLinea(color, xinicial , yinicial , xfinal , yfinal, lienzo)
 {
   lienzo.beginPath();/*iniciar un trazo en js*/
   lienzo.strokeStyle = color; /*variable que permite elegir tipo de trazo y color*/
@@ -46,9 +58,11 @@ function dibujarLinea(color, xinicial , yinicial , xfinal , yfinal)
   lienzo.stroke();/*realizar linea, circulo, forma, etc*/
   lienzo.closePath();/*terminar un trazo*/
 }
+ dibujarLinea("red", 150, 150, 151, 151, papel);
+
 /*--Dibujo--*/
 /*Bordes*/
-dibujarLinea("black", 0,0,0, 300);
-dibujarLinea("black", 0 , 300, 300,300);
-dibujarLinea("black" ,0,0,300, 0 );
-dibujarLinea("black", 300,0 , 300, 300);
+dibujarLinea("black", 0,0,0, 300, papel);
+dibujarLinea("black", 0 , 300, 300,300, papel);
+dibujarLinea("black" ,0,0,300, 0, papel );
+dibujarLinea("black", 300,0 , 300, 300, papel);
